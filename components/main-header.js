74643d46347741315076fb92f0bda8cc28fc5986
Roberto <roberto.quadraccia90@gmail.com>
2024-02-10 17:11:40 +0100

    Image replaced the img element

--- components/main-header.js
+++ components/main-header.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import logoImg from "@/assets/logo.png";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png"; // this would be an object in NextJS
 import classes from "./main-header.module.css";
 
 const MainHeader = () => {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         The Foodies App
       </Link>
 
