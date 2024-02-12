095966398f4478162d03f3dc061ecca1f1ff292e
Roberto <roberto.quadraccia90@gmail.com>
2024-02-12 10:02:58 +0100

    Server action implemented to handle form submission

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,28 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 const ShareMealPage = () => {
+  const shareMeal = async (formData) => {
+    // This creates a Server Action which is a function that is guaranteed
+    // to execute on the server, and only there.
+    // In addition, to really turn this into a so-called Server Action,
+    // you also must add the "async" keyword in front of it.
+    // We now assign shareMeal as a value on the action prop on the form.
+    "use server";
+
+    // formData is the data entered in the form, we can now destructure it with .get()
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email")
+    };
+
+    console.log(meal)
+  };
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +33,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +61,7 @@ const ShareMealPage = () => {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
