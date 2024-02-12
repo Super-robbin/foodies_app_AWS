f75e2ae41980918724daf05c953d81846802757a
Roberto <roberto.quadraccia90@gmail.com>
2024-02-12 10:24:22 +0100

    Added slugify and xss as dependancies and used to save meal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,28 +1,9 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/action";
 
 const ShareMealPage = () => {
-  const shareMeal = async (formData) => {
-    // This creates a Server Action which is a function that is guaranteed
-    // to execute on the server, and only there.
-    // In addition, to really turn this into a so-called Server Action,
-    // you also must add the "async" keyword in front of it.
-    // We now assign shareMeal as a value on the action prop on the form.
-    "use server";
-
-    // formData is the data entered in the form, we can now destructure it with .get()
-
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email")
-    };
-
-    console.log(meal)
-  };
+  
 
   return (
     <>
