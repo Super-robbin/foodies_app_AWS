02cf4326cb1d5e761c46e1c43124754a1e32d880
Roberto <roberto.quadraccia90@gmail.com>
2024-02-19 15:09:42 +0100

    Metadata added both statically and dynamically

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+}; // Statically generated metadata
+
 // Instead of using loading.js, we create the Meals component below which is responsible
 // for returning the MealsGrid. So we will cut the MealsGrid from the MealsPage component
 // and return it here inside Meals, we then use Meals inside MealsPage.
